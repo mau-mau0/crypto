2e7ce5d0765a00545dc7a728beca6d4a37d88df7
maumau <k.maumau11@gmail.com>
2022-06-06 11:05:27 -0500

    small update

--- service/main.py
+++ service/main.py
@@ -1,64 +1,11 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 
+# html_file = open("/crypto/service/webpage/index.html", "r")
+html_file = open("F:\Documents\Programming\pricelistener\crypto\service\webpage", "r")
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Kucoin Websocket</title>
-    </head>
-    <body>
-        <h1>Kucoin Websocket</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-            <li>Enter Channel Subscription Above</li>
-        </ul>
-        <script>
-            var ws = new WebSocket("wss://ws-api.kucoin.com/endpoint?token=2neAiuYvAU61ZDXANAGAsiL4-iAExhsBXZxftpOeh_55i3Ysy2q2LEsEWU64mdzUOPusi34M_wGoSf7iNyEWJwjDpKR3zLnc_qURhouO340p6s6oMZBeddiYB9J6i9GjsxUuhPw3BlrzazF6ghq4L0SqSvVGXBYse3OKoHhJp0I=.epP4twHtbbyzniXlloGupg==");
-
-            // Connection opened
-            ws.addEventListener('open', function (event) {
-                console.log('Hello Server!');
-                setInterval(function(){
-                    sendMsg(event=event,
-                            input = {
-                                        "id":"1545910590801",
-                                        "type":"ping"
-                                    }
-                    )
-                }, 4999)
-            });
-
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-
-            function sendMsg(event, input) {
-                var input = input
-                ws.send(JSON.stringify(input))
-                input.value = ''
-                event.preventDefault()
-                console.log(JSON.stringify(input))
-            }
-        </script>
-    </body>
-</html>
-"""
+html = html_file.read()
 
 
 @app.get("/")
